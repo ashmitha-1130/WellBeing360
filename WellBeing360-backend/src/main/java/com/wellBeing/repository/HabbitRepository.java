35febc6d7728e22fe10e762b66afb8c36562bb64
ashmitha <ashmithasunil37@gmail.com>
2025-09-08 19:34:48 +0530

    Added Habit repository and service code

--- WellBeing360-backend/src/main/java/com/wellBeing/repository/HabbitRepository.java
+++ WellBeing360-backend/src/main/java/com/wellBeing/repository/HabbitRepository.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 import com.wellBeing.entity.Habbit;
 
 @Repository
-public interface HabbitRepository extends JpaRepository<Habbit,Integer> {
+public interface HabbitRepository extends JpaRepository<Habbit,Long> {
+	
+	
 
 }
