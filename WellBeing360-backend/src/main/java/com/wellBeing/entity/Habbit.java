8672534b9fe9798cbb2f9464bbc8326b77ec3046
ashmitha <ashmithasunil37@gmail.com>
2025-09-08 19:03:37 +0530

    updated habit entity

--- WellBeing360-backend/src/main/java/com/wellBeing/entity/Habbit.java
+++ WellBeing360-backend/src/main/java/com/wellBeing/entity/Habbit.java
@@ -1,18 +1,41 @@
 package com.wellBeing.entity;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(
+		name="habbit",
+		uniqueConstraints = {
+				@UniqueConstraint(columnNames = {"user_id","name"})
+		}
+		
+		)
 public class Habbit {
+	
+	public enum Frequency{
+		Daily,
+		Weekly
+	}
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
+    private long habbit_id;
+    @Column(nullable = false)
     private String name;
-    private String description;
-
+    
+    @ManyToOne
+    @JoinColumn(name="user_id",nullable=false)
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable =false)
+    private Frequency frequency;
+    
+    @Column(nullable = false)
+    private int streak_count;
+    private LocalDate last_completed_date;
  
 }
